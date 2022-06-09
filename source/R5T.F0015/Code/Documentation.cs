fe0a67e9e001a0c831825367f6e8d69182328286
davidcoats <david@getsafetycone.com>
2022-06-09 12:26:18 -0700

    Interim changes.

--- source/R5T.F0015/Code/Documentation.cs
+++ source/R5T.F0015/Code/Documentation.cs
@@ -4,7 +4,7 @@ using System;
 namespace R5T.F0015
 {
     /// <summary>
-    /// Rivet-specific, top-level, syntax element generation functionality returning CompilationRequirements<T> instances.
+    /// Rivet-specific, top-level, syntax element generation functionality returning CompilationRequirements{T} instances.
     /// </summary>
     public static class Documentation
     {
